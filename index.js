51b152e21ae0e4ceddeeb49e83624c47eb980bea
Olivia Osborn <olioz95@gmail.com>
2019-02-11 17:27:33 -0500

    added POST and DELETE requests

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const db = require("./data/db.js");
 const server = express();
 server.use(express.json());
 
+// POST /api/users:
+server.post("/api/users", (req, res) => {
+    const newUser = req.body;
+    db
+        .insert(newUser)
+        .then(users => {
+            if (!newUser.name || !newUser.bio) {
+                res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+            } else {
+                res.status(201).json({users})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+});
+
 // GET /api/users:
 server.get("/api/users", (req, res) => {
     db
@@ -33,7 +50,22 @@ server.get("/api/users/:id", (req, res) => {
         })
 })
 
-
+// DELETE /api/users/:id:
+server.delete("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    db
+        .remove(id)
+        .then(users => {
+            if (!id) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            } else {
+                res.status(200).json({users})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user could not be removed" })
+        })
+})
 
 server.listen(4000, () => {
     console.log("\n*** Running on port 4000 ***\n")
